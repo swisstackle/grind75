a10c79cd6d5686dee71776395307ae37ee02f02d
Alain Georges Schaerer <alainsch@buffalo.edu>
2023-02-28 12:36:18 -0500

    NEW: Linked List Cycle

---
+++ linkedlistcycle/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+bool hasCycle(struct ListNode *head) {
+    if(head == NULL) {
+        return false;
+    }
+    struct ListNode *slow = head;
+    struct ListNode *fast = head;
+
+    while(fast != NULL && fast->next != NULL) {
+        fast = fast->next->next;
+        slow = slow->next;
+		if(slow != NULL && fast != NULL) {
+			if(slow->val == fast->val) {
+				return true;
+			}
+		}
+    }
+    return false;
+}
+
+int main() {
+	struct ListNode head;
+	struct ListNode two;
+	struct ListNode zero;
+	struct ListNode four;
+
+	head.val = 3;
+	two.val = 2;
+	zero.val = 0;
+	four.val = 4;
+
+	head.next = &two;
+	two.next = &zero;
+	zero.next = &four;
+	four.next = &two;
+	
+	printf("%d", hasCycle(&head));
+}
