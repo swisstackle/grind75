304b088125fe80e220695dd0f6f25d873b7efec7
Alain Georges Schaerer <alainsch@buffalo.edu>
2023-03-03 14:37:01 -0500

    NEW: firstBadVersion

---
+++ firstbad/README.md
@@ -0,0 +1,4 @@
+# Exercise
+https://leetcode.com/problems/first-bad-version/
+# Explanation
+We just use a modified version of binary search. It's self explanatory for every programmer.
---
+++ firstbad/main.c
@@ -0,0 +1,22 @@
+// Note: the function isBadVersion() is given as a black box on Leetcode. Therefore this exerc
+// ise doesn't contain a Makefile or a main method.
+int firstBadVersion(int n) {
+	if(isBadVersion(0)) {
+		return 0;
+	}
+   int start = 1, end = n;
+    while(start <= end){
+        int mid = start + (end - start)/2;
+        if(isBadVersion(mid) && !(isBadVersion(mid-1))){
+            return mid;
+        }
+        else if(!(isBadVersion(mid))){
+            start = mid + 1;
+        }
+        else{
+            end = mid - 1;
+        }
+    }
+    return -1;
+}
+
